c66dbcc7a152834d683e07132f9436638c3e282c
Jack Lach <jack.lach4@gmail.com>
2023-03-21 23:34:12 -0400

    create and test createTimeInEvent() and createTimeOutEvent()

--- index.js
+++ index.js
@@ -21,12 +21,30 @@ function createEmployeeRecords([...records]) {
     return arrOfEmployeeObjects;
 };
 
-function createTimeInEvent() {
-
+function createTimeInEvent(emplRecordObj, timeStamp) {
+    const hour = timeStamp[11] + timeStamp[12];
+    const date = timeStamp.slice(0,10);
+    
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: hour,
+        date: date,
+    };
+    emplRecordObj.timeInEvents.push(timeInEvent);
+    return emplRecordObj;
 };
 
-function createTimeOutEvent() {
-
+function createTimeOutEvent(emplRecordObj, timeStamp) {
+    const hour = timeStamp[11] + timeStamp[12];
+    const date = timeStamp.slice(0,10);
+    
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: hour,
+        date: date,
+    };
+    emplRecordObj.timeOutEvents.push(timeOutEvent);
+    return emplRecordObj;
 };
 
 function hoursWorkedOnDate() {
