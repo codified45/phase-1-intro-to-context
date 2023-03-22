2cd80b2356da3a58505b65dce551fb3f3845fc7d
Jack Lach <jack.lach4@gmail.com>
2023-03-21 22:08:52 -0400

    create and test createEmployeeRecord() and createEmployeeRecords()

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Your code here
+
+
+function createEmployeeRecord([firstName, familyName, title, payPerHour]) {
+    const employeeRecord = {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return employeeRecord;
+};
+
+function createEmployeeRecords([...records]) {
+    const employeesArray = records;
+    let arrOfEmployeeObjects = []
+    for (const employeeArray of employeesArray) {
+        arrOfEmployeeObjects.push(createEmployeeRecord(employeeArray));
+    };
+    return arrOfEmployeeObjects;
+};
+
+function createTimeInEvent() {
+
+};
+
+function createTimeOutEvent() {
+
+};
+
+function hoursWorkedOnDate() {
+
+};
+
+function wagesEarnedOnDate() {
+
+};
+
+function allWagesfor() {
+
+};
+
+function calculatePayroll() {
+
+};
